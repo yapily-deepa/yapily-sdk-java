940032421a3df0454c6b929befc91166558d38eb
Dexter Awoyemi <dexterawoyemi@Dexters-MacBook-Pro.local>
2018-03-15 09:51:52 +0000

    Match renamed endpoints

--- sdk/src/main/java/yapily/sdk/YapilyApi.java
+++ sdk/src/main/java/yapily/sdk/YapilyApi.java
@@ -24,9 +24,9 @@ public class YapilyApi {
   public static final String SERVICE_PATH_BANKS = "/banks";
   public static final String SERVICE_PATH_USERS = "/users";
 
-  public static final String SERVICE_PATH_USERS_BANKS_IDENTITY = "/user/{userUuid}/banks/{bankId}/identity";
-  public static final String SERVICE_PATH_USERS_BANKS_ACCOUNTS = "/user/{userUuid}/banks/{bankId}/accounts";
-  public static final String SERVICE_PATH_USERS_BANKS_ACCOUNTS_TRANSACTIONS = "/user/{userUuid}/banks/{bankId}/accounts/{accountId}/transactions";
+  public static final String SERVICE_PATH_USERS_BANKS_IDENTITY = "/users/{userUuid}/banks/{bankId}/identity";
+  public static final String SERVICE_PATH_USERS_BANKS_ACCOUNTS = "/users/{userUuid}/banks/{bankId}/accounts";
+  public static final String SERVICE_PATH_USERS_BANKS_ACCOUNTS_TRANSACTIONS = "/users/{userUuid}/banks/{bankId}/accounts/{accountId}/transactions";
 
   public static final String API_APPLICATION_ID_ENV_NAME = "YAPILY_API_APPLICATION_ACCESS_KEY";
   public static final String API_APPLICATION_SECRET_ENV_NAME = "YAPILY_API_APPLICATION_ACCESS_SECRET";
